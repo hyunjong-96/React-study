1ed0c793b56777a3af99c94da57e745a1047beb0
leehyunjong <ahajongs@gmail.com>
2020-12-27 17:18:36 +0900

    ch.16 useEffect Hook

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 function User({user,index,onRemove,onToggle}){
+    useEffect(()=>{
+        console.log('user값이 설정됨')
+        console.log(user)
+        return()=>{
+            console.log('user가 바뀌기전..')
+            console.log(user)
+        }
+    },[user])
+    const testClick=()=>{
+        console.log('!!!!!!!!!!!!!!!!!')
+    }
     return(
         <div>
             <b
@@ -11,11 +22,20 @@ function User({user,index,onRemove,onToggle}){
                 onClick={()=>onToggle(user.id)}
             >{user.id} {user.username}</b> <span>({user.email})</span>
             <button onClick={()=>onRemove(user.id)}>삭제</button>
+            <button onClick={testClick}>test</button>
         </div>
     )
 }
 
 function UserList({users,onRemove,onToggle}) {
+    useEffect(()=>{
+        console.log('users부모컴포넌트')
+        console.log(users)
+        return()=>{
+            console.log('users여긴 뭘까')
+            console.log(users)
+        }
+    },[users])
     return(
         <div>
             {users.map((user,index)=>(
